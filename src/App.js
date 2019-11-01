5fe3d39e23e198ad3c39b92283d3930d8cba9b8f
Kira <kawoods826@gmail.com>
2019-11-01 11:15:40 -0700

    added restaurant info to home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import { restaurant_data } from "./sf-restaurant-data";
 function App() {
   return (
     <div className="App">
-      <p>{JSON.stringify(restaurant_data)}</p>
+      {restaurant_data.map(restaurant => (
+        <div>
+          <h1>{restaurant.business_name}</h1>
+          <p>
+            {restaurant.business_address}, {restaurant.business_city},{" "}
+            {restaurant.business_state}, {restaurant.business_postal_code}
+          </p>
+          <p>{restaurant.risk_category}</p>
+        </div>
+      ))}
     </div>
   );
 }
