8077f24be7ddd3fc076d7b3005bcbe447129f89b
Kira <kawoods826@gmail.com>
2019-11-04 11:39:45 -0800

    converted remaining functional components to class components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import NavBar from "./components/NavBar";
 
 import { Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="app-container">
-      <NavBar />
-      <Route exact path="/" component={WelcomePage} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <NavBar />
+        <Route exact path="/" component={WelcomePage} />
+      </div>
+    );
+  }
 }
 
 export default App;
