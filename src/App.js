8229464984476dd5f3b1142064e10bcecd9c96b9
Kira <kawoods826@gmail.com>
2019-11-03 23:15:37 -0800

    added info to welcome page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
+import { WelcomePage } from "./components/WelcomePage";
 
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app-container">
-      <Header />
+      <WelcomePage />
     </div>
   );
 }
