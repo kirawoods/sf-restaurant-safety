0ea874f88ec78f8201e81ec2e9b58b1b9be2d41a
Kira <kawoods826@gmail.com>
2019-11-04 21:18:05 -0800

    dissociated renderRestaurant function from render funciton

--- src/components/MostRecentInspections.js
+++ src/components/MostRecentInspections.js
@@ -11,30 +11,33 @@ const sortedRestaurantData = restaurant_data.sort((a, b) =>
 const mostRecentRestaurants = sortedRestaurantData.slice(0, 6);
 
 class MostRecentInspections extends Component {
+  renderRestaurant(restaurant) {
+    return (
+      <Card
+        className="restaurant-card"
+        key={restaurant.inspection_id}
+        elevation={Elevation.THREE}
+      >
+        <h1>{restaurant.business_name}</h1>
+
+        <p>
+          {restaurant.business_address}, {restaurant.business_city},
+          {restaurant.business_state}, {restaurant.business_postal_code}
+        </p>
+
+        <p>
+          Inspection Date:{" "}
+          {moment(restaurant.inspection_date).format("MMM Do[,] YYYY")}
+        </p>
+
+        <p>{restaurant.risk_category}</p>
+      </Card>
+    );
+  }
   render() {
     return (
       <div className="restaurant-info">
-        {mostRecentRestaurants.map(restaurant => (
-          <Card
-            className="restaurant-card"
-            key={restaurant.inspection_id}
-            elevation={Elevation.THREE}
-          >
-            <h1>{restaurant.business_name}</h1>
-
-            <p>
-              {restaurant.business_address}, {restaurant.business_city},
-              {restaurant.business_state}, {restaurant.business_postal_code}
-            </p>
-
-            <p>
-              Inspection Date:{" "}
-              {moment(restaurant.inspection_date).format("MMM Do[,] YYYY")}
-            </p>
-
-            <p>{restaurant.risk_category}</p>
-          </Card>
-        ))}
+        {mostRecentRestaurants.map(this.renderRestaurant)}
       </div>
     );
   }
