eb27aeead7e1de267f10b2f750666e2458d3b0e6
Kira <kawoods826@gmail.com>
2019-11-06 11:07:54 -0800

    added basic search functionality with console log

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 import { Button, Classes } from "@blueprintjs/core";
 import "./SearchForm.css";
+import { restaurant_data } from "../sf-restaurant-data";
 
-export default function Search(props) {
-  const [searchResults, setSearchResults] = useState([]);
+export default function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState(restaurant_data);
+  const handleChange = event => {
+    event.preventDefault();
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    const filteredInspections = searchResults.filter(x =>
+      x.business_name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredInspections);
+    console.log(filteredInspections);
+  };
+
   return (
     <div className="search-container">
-      <form className="search-form" onSubmit={props.handleSubmit}>
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           className={Classes.INPUT + " search-input"}
           name="search"
           type="text"
           placeholder="Search by Restaurant Name..."
-          onChange={props.handleChange}
+          onChange={handleChange}
         />
         <Button className="search-button" type="submit">
           Search
