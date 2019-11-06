da5024944e48c9e27157c5b0a872b61513e8694f
Kira <kawoods826@gmail.com>
2019-11-06 11:20:07 -0800

    made search update with each keystroke

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,24 +5,21 @@ import { restaurant_data } from "../sf-restaurant-data";
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState(restaurant_data);
+  const [searchResults, setSearchResults] = useState(searchTerm);
   const handleChange = event => {
     event.preventDefault();
     console.log(event.target.value);
     setSearchTerm(event.target.value);
-  };
-  const handleSubmit = event => {
-    event.preventDefault();
-    const filteredInspections = searchResults.filter(x =>
+    const filteredInspections = restaurant_data.filter(x =>
       x.business_name.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    setSearchResults(filteredInspections);
+    setSearchResults(filteredInspections[0].business_name);
     console.log(filteredInspections);
   };
 
   return (
     <div className="search-container">
-      <form className="search-form" onSubmit={handleSubmit}>
+      <form className="search-form">
         <input
           className={Classes.INPUT + " search-input"}
           name="search"
@@ -30,11 +27,8 @@ export default function Search() {
           placeholder="Search by Restaurant Name..."
           onChange={handleChange}
         />
-        <Button className="search-button" type="submit">
-          Search
-        </Button>
       </form>
-      <div className="search-results"></div>
+      <div className="search-results">{searchResults}</div>
     </div>
   );
 }
